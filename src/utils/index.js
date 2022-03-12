c9cff7d37ccd096d9a45ec1b5be91dcf2497dc2b
Galih Akbar <galihakbar.ga91@gmail.com>
2022-03-12 12:45:03 +0700

    user registration implemented

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,12 @@
 /* eslint-disable camelcase */
-const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  created_at,
+  updated_at,
+}) => ({
   id,
   title,
   body,
